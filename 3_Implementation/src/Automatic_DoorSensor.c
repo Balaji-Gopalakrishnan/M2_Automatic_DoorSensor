120e9d9534c915424d70cc692034f13f2a3ecd73
Balaji-Gopalakrishnan <74197288+Balaji-Gopalakrishnan@users.noreply.github.com>
2022-04-21 22:11:59 +0530

    Update Automatic_DoorSensor.c

--- 3_Implementation/src/Automatic_DoorSensor.c
+++ 3_Implementation/src/Automatic_DoorSensor.c
@@ -1,16 +1,25 @@
-#define F_CPU 16000000
-#include <avr/io.h>
-#include <stdio.h>
+/*
+ * Automatic_DoorSensor.c
+ *  author BALAJI G 
+ *  version 0.1
+ *  date 2022-04-21
+ *
+ *
+ * copyright Copyright (c) 2022
+ */   
+
+#include <avr/io.h>   // This header file includes the appropriate Input/output definitions for the device
+#include <stdio.h>    //stdio. h is a header file which has the necessary information to include the input/output related functions in our program.
 int main(void)
 {
-  DDRB=DDRB&0b11111101;
-  DDRD=DDRD|0b01000000;
+  DDRB=DDRB&0b11111101;      // we have connect the switch on the port B
+  DDRD=DDRD|0b01000000;     // we have connect the Voltmeter and Led  on the port D 
   while(1)
   {
-if(PINB & 0b00000010)
-  PORTD=PORTD|0b01000000;
+if(PINB & 0b00000010)        ///toggle pin 1 of B using PINB "input write tricks
+  PORTD=PORTD|0b01000000;    // Used to configure the respective PORT d as output/input
     else
-    PORTD=PORTD&0b10111111;
+    PORTD=PORTD&0b10111111;  // when door closed the led turns off and the buzzer gets ON
   }
   return 0;
   }
